3d84246b1458edb7f65cb7dcaa7a2685cbb0ea33
Gabby <zsalorenhilliard@gmail.com>
2021-10-16 19:40:48 -0400

    Your Message

---
+++ scripts.js
@@ -0,0 +1,30 @@
+var userChoice;
+var computerChoice;
+var randomNumber;
+var globalWins
+var globalLoss
+var globalTies
+//Returns a random number
+Math.floor(Math.random() * 2);
+
+prompt("How many rounds do you want to play? "); {
+if(userChoice = "3")
+    prompt("Do you pick rock, paper, or scissors? (enter r, p, or s). ") 
+if(userChoice = "r") 
+    alert("It's a tie! "); 
+}
+//This is a global wins, the user wins//
+ prompt("Do you pick rock, paper, or scissors? (enter r, p, or s). "); 
+if(userChoice = "p")
+alert("You win! You picked p which beats r ");
+//This is a global tie, the user tied//
+prompt("Do you pick rock, paper, or scissors? (enter r, p, or s). "); {
+if(userChoice = "s")
+alert("It's a tie! ");
+}
+//This is a global loss, the user lost the game//
+prompt("Do you pick rock, paper, or scissors? (enter r, p, or s). "); {
+if(userChoice = "r")
+alert("You loser! The computer picked p which beats r ");
+}
+alert("You won one time, you lost one time, and you tied twice ");
